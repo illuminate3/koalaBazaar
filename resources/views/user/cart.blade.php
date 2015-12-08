92ed859de012c924ab8817b4ebd5dab0a7da77ea
Ömer Faruk Aladağ <ofaladag@gmail.com>
2015-12-08 21:47:21 +0200

    asdasd

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -13,7 +13,21 @@
     <div class="content-wrap">
 
         <div class="container clearfix">
+            @if($errors->has())
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger">
+                        {{ $error }}
+                    </div>
+                @endforeach
+            @endif
 
+            @if(\Illuminate\Support\Facades\Session::has('success'))
+                @foreach (\Illuminate\Support\Facades\Session::pull('success') as $success)
+                    <div class="alert alert-success">
+                        {{ $success }}
+                    </div>
+                @endforeach
+            @endif
             <div class="table-responsive bottommargin">
 
                 <table class="table cart">
@@ -21,115 +35,53 @@
                     <tr>
                         <th class="cart-product-remove">&nbsp;</th>
                         <th class="cart-product-thumbnail">&nbsp;</th>
-                        <th class="cart-product-name">Product</th>
-                        <th class="cart-product-price">Unit Price</th>
-                        <th class="cart-product-quantity">Quantity</th>
-                        <th class="cart-product-subtotal">Total</th>
+                        <th class="cart-product-name">Ürün</th>
+                        <th class="cart-product-price">Birim Fiyatı</th>
+                        <th class="cart-product-quantity">Adet</th>
+                        <th class="cart-product-subtotal">Toplam</th>
                     </tr>
                     </thead>
                     <tbody>
+                    @foreach($products as $product)
+                        <?php $productObject=\App\Product::find($product->product_id)->first() ;?>
                     <tr class="cart_item">
                         <td class="cart-product-remove">
-                            <a href="#" class="remove" title="Remove this item"><i class="icon-trash2"></i></a>
+                            <a href="{{ action('Frontend\ProductController@removeFromCart',$product->product_id) }}" class="remove" title="Bu ürünü kaldır."><i class="icon-trash2"></i></a>
                         </td>
 
                         <td class="cart-product-thumbnail">
-                            <a href="#"><img width="64" height="64" src="images/shop/thumbs/small/dress-3.jpg" alt="Pink Printed Dress"></a>
+                            <a href="{{ action('Frontend\ProductController@show',$product->product_id) }}"><img width="64" height="64" src="{{ action('FileEntryController@show',$productObject->image) }}"></a>
                         </td>
 
                         <td class="cart-product-name">
-                            <a href="#">Pink Printed Dress</a>
+                            <a href="{{ action('Frontend\ProductController@show',$product->product_id) }}">{{ $productObject->title }}</a>
                         </td>
 
                         <td class="cart-product-price">
-                            <span class="amount">$19.99</span>
+                            <span class="amount">{{ $productObject->price }}</span>
                         </td>
 
                         <td class="cart-product-quantity">
                             <div class="quantity clearfix">
-                                <input type="button" value="-" class="minus">
-                                <input type="text" name="quantity" value="2" class="qty" />
-                                <input type="button" value="+" class="plus">
+                                <input type="button" onclick="window.open('{{ action('Frontend\ProductController@removeFromCart',['id'=>$product->product_id,'quantity'=>1]) }}','_self')"  value="-" class="minus">
+                                <input type="text" name="quantity" disabled value="{{ $product->order_number }}" class="qty" />
+                                <input type="button" onclick="window.open('{{ action('Frontend\ProductController@addToCart',$product->product_id) }}','_self')" value="+" class="plus">
                             </div>
                         </td>
 
                         <td class="cart-product-subtotal">
-                            <span class="amount">$39.98</span>
+                            <span class="amount">{{ $product->order_number*$productObject->price }}</span>
                         </td>
                     </tr>
-                    <tr class="cart_item">
-                        <td class="cart-product-remove">
-                            <a href="#" class="remove" title="Remove this item"><i class="icon-trash2"></i></a>
-                        </td>
+                    @endforeach
 
-                        <td class="cart-product-thumbnail">
-                            <a href="#"><img width="64" height="64" src="images/shop/thumbs/small/shoes-2.jpg" alt="Checked Canvas Shoes"></a>
-                        </td>
-
-                        <td class="cart-product-name">
-                            <a href="#">Checked Canvas Shoes</a>
-                        </td>
-
-                        <td class="cart-product-price">
-                            <span class="amount">$24.99</span>
-                        </td>
-
-                        <td class="cart-product-quantity">
-                            <div class="quantity clearfix">
-                                <input type="button" value="-" class="minus">
-                                <input type="text" name="quantity" value="1" class="qty" />
-                                <input type="button" value="+" class="plus">
-                            </div>
-                        </td>
-
-                        <td class="cart-product-subtotal">
-                            <span class="amount">$24.99</span>
-                        </td>
-                    </tr>
-                    <tr class="cart_item">
-                        <td class="cart-product-remove">
-                            <a href="#" class="remove" title="Remove this item"><i class="icon-trash2"></i></a>
-                        </td>
-
-                        <td class="cart-product-thumbnail">
-                            <a href="#"><img width="64" height="64" src="images/shop/thumbs/small/tshirt-2.jpg" alt="Pink Printed Dress"></a>
-                        </td>
-
-                        <td class="cart-product-name">
-                            <a href="#">Blue Men Tshirt</a>
-                        </td>
-
-                        <td class="cart-product-price">
-                            <span class="amount">$13.99</span>
-                        </td>
-
-                        <td class="cart-product-quantity">
-                            <div class="quantity clearfix">
-                                <input type="button" value="-" class="minus">
-                                <input type="text" name="quantity" value="3" class="qty" />
-                                <input type="button" value="+" class="plus">
-                            </div>
-                        </td>
-
-                        <td class="cart-product-subtotal">
-                            <span class="amount">$41.97</span>
-                        </td>
-                    </tr>
-                    <tr class="cart_item">
-                        <td colspan="6">
-                            <div class="row clearfix">
-                                <div class="col-md-4 col-xs-4 nopadding"></div>
-                                <div class="col-md-8 col-xs-8 nopadding">
-                                    <a href="#" class="button button-3d nomargin fright">Update Cart</a>
-                                    <a href="shop.html" class="button button-3d notopmargin fright">Proceed to Checkout</a>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
                     </tbody>
 
                 </table>
 
+            </div>
+            <div class="col-md-12 col-xs-12 nopadding">
+                <a href="shop.html" class="button button-3d notopmargin fright">Proceed to Checkout</a>
             </div>
 
 
