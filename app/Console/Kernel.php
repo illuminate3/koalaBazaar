89034d48cd1d5733e9766614f49efe506c823a9c
Ömer Faruk Aladağ <ofaladag@gmail.com>
2015-12-16 19:41:51 +0200

    asdasd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('addtocart:comment')->everyFiveMinutes();
+        $schedule->command('addtocart:comment')->everyMinute();
     }
 }
