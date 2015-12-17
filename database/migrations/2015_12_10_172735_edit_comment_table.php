22f4787999d2f490ad5fc49034456b1020a19de8
Ömer Faruk Aladağ <ofaladag@gmail.com>
2015-12-17 20:53:32 +0200

    asdasd

--- database/migrations/2015_12_10_172735_edit_comment_table.php
+++ database/migrations/2015_12_10_172735_edit_comment_table.php
@@ -17,11 +17,6 @@ class EditCommentTable extends Migration
             $table->integer('user_id')->unsigned()->nullable();
             $table->foreign("user_id")->references("id")->on("users")->onDelete('set null');
         });
-        Schema::table('rankings', function (Blueprint $table) {
-            $table->dropColumn('customer_id');
-            $table->integer('user_id')->unsigned()->nullable();
-            $table->foreign("user_id")->references("id")->on("users")->onDelete('set null');
-        });
     }
 
     /**
@@ -32,11 +27,6 @@ class EditCommentTable extends Migration
     public function down()
     {
         Schema::table('comments', function (Blueprint $table) {
-            $table->dropColumn('user_id');
-            $table->integer('customer_id')->unsigned()->nullable();
-            $table->foreign("customer_id")->references("id")->on("customers")->onDelete('set null');
-        });
-        Schema::table('rankings', function (Blueprint $table) {
             $table->dropColumn('user_id');
             $table->integer('customer_id')->unsigned()->nullable();
             $table->foreign("customer_id")->references("id")->on("customers")->onDelete('set null');
